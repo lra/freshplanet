6247f4d67158a047f54a41bf3910f1cf0970947c
Laurent Raufaste <analogue@glop.org>
2010-08-17 00:33:23 +0200

    Creation of the minesweeper aplication

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    minesweeper
+ * @subpackage form
+ * @author     Laurent Raufaste 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
