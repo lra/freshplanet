db48d4c6251e9731e2314eb555a4cf437c39598e
Laurent Raufaste <analogue@glop.org>
2010-08-17 22:13:46 +0200

    Added the admin modules for User and Hiscore

---
+++ apps/backend/modules/hiscore/lib/hiscoreGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * hiscore module configuration.
+ *
+ * @package    minesweeper
+ * @subpackage hiscore
+ * @author     Laurent Raufaste
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class hiscoreGeneratorConfiguration extends BaseHiscoreGeneratorConfiguration
+{
+}
---
+++ apps/backend/modules/hiscore/lib/hiscoreGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * hiscore module helper.
+ *
+ * @package    minesweeper
+ * @subpackage hiscore
+ * @author     Laurent Raufaste
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class hiscoreGeneratorHelper extends BaseHiscoreGeneratorHelper
+{
+}
