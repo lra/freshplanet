4006fccfe694a322363e5e1695197fb3594ade33
Laurent Raufaste <analogue@glop.org>
2010-08-20 17:11:00 +0200

    Display a title and some data for the account page

--- apps/frontend/modules/user/templates/loginSuccessfulSuccess.php
+++ apps/frontend/modules/user/templates/loginSuccessfulSuccess.php
@@ -1,4 +1,13 @@
+<h1>Your account</h1>
+
 <div class="message">
 	 <p>Welcome <?php echo $dbUser->getFirstname(); ?>, you've successfully logged in the game =)</p>
+
+   <ul>
+     <li>Firstname: <?php echo $dbUser->getFirstname(); ?></li>
+     <li>Lastname: <?php echo $dbUser->getLastname(); ?></li>
+     <li>Email: <a href="mailto:<?php echo $dbUser->getEmail(); ?>"><?php echo $dbUser->getEmail(); ?></a></li>
+   </ul>
+
    <p>You can now start playing a game or continue to play your last gameboard.</p>
 </div>
