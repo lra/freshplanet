222937570c011d766cdab2ffd1bb6b1f08f38e42
Laurent Raufaste <analogue@glop.org>
2010-08-20 11:49:08 +0200

    Basic message design

--- apps/frontend/modules/user/templates/loginSuccessfulSuccess.php
+++ apps/frontend/modules/user/templates/loginSuccessfulSuccess.php
@@ -1,3 +1,4 @@
-<p>Welcome <?php echo $dbUser->getFirstname(); ?>, you've successfully logged in the game =)</p>
-
-<p>You can now start playing a game or continue to play your last gameboard.</p>
+<div class="message">
+	 <p>Welcome <?php echo $dbUser->getFirstname(); ?>, you've successfully logged in the game =)</p>
+   <p>You can now start playing a game or continue to play your last gameboard.</p>
+</div>
