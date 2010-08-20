50eb4b636ec7ceaefe5e5175bae85a09df24f796
Laurent Raufaste <analogue@glop.org>
2010-08-20 17:56:08 +0200

    Added a fake primary gameboard

--- apps/frontend/modules/game/templates/indexSuccess.php
+++ apps/frontend/modules/game/templates/indexSuccess.php
@@ -1,3 +1,13 @@
 <h2>Game Board</h2>
 
 <p class="message">The game board will be displayed here</p>
+
+<table class="board">
+	 <?php for ($y = 1; $y <= 10; $y++): ?>
+	 <tr>
+	 <?php for ($x = 1; $x <= 10; $x++): ?>
+   <td>O</td>
+	 <?php endfor; ?>
+	 </tr>
+	 <?php endfor; ?>
+</table>
